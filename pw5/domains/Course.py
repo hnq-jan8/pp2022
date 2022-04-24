91f2d8e33e65daa81c4aa82460ce6882037e966e
hnq-jan8 <nqhien.tm02@outlook.com>
2022-04-25 03:58:43 +0700

    Revert "Labwork 5 finish (#1)"
    This reverts commit 6148aa50300f1b4b4ca8a90b54283d497815c2cf.
    
    It was a mistake

--- pw5/domains/Course.py
+++ pw5/domains/Course.py
@@ -1,21 +1,13 @@
 from domains.Entity import Entity
 
 class Course(Entity):
-    def __init__(self, id = None, name = None, credits = None):
-        if id == None:
-            super().__init__()
-            c = input('. Enter course credits: ')
-            while not c.isdigit() or int(c) < 1:
-                c = input(f'''  (!) Invalid number for credits
-                    \r     Enter again course credits: ''')
-            self.__credits = int(c)
-        else:
-            self.set_id(id)
-            self.set_name(name)
-            self.__credits = int(credits)
-
-    def __repr__(self):
-        return f"Course('{self.get_id()}', '{self.get_name()}', '{self.__credits}')"
+    def __init__(self):     # id, name, credits
+        super().__init__()
+        c = input('. Enter course credits: ')
+        while not c.isdigit() or int(c) < 1:
+            c = input(f'''  (!) Invalid number for credits
+                \r     Enter again course credits: ''')
+        self.__credits = int(c)
 
     def get_credits(self):
         return self.__credits
--- pw5/domains/Student.py
+++ pw5/domains/Student.py
@@ -1,27 +1,14 @@
 from domains.Entity import Entity
 
 class Student(Entity):
-    def __init__(self, id = None, name = None, DoB = None, mark = None):
-        if id == None:
-            super().__init__()
-            DoB = input(f'. Enter {(type(self).__name__).lower()} DoB: ')
-            while DoB == '':
-                DoB = input('  (!) Student DoB is required! Try again: ')
-            self.__DoB = DoB
-            self.__marks = []
-        else:
-            self.set_id(id)
-            self.set_name(name)
-            self.__DoB = DoB
-            self.__marks = mark
+    def __init__(self):     # id, name, DoB, gpa
+        super().__init__()
+        self.__DoB = input(f'. Enter {(type(self).__name__).lower()} DoB: ')
+        self.__marks = []   # List of marks for each course
         self.__gpa = 0
 
-    def __repr__(self) -> str:
-        return f"Student('{self.get_id()}', '{self.get_name()}', '{self.__DoB}', {self.__marks})"
-
-    def add_course(self, num_course):
-        for _ in range(num_course):
-            self.__marks.append(-1)
+    def add_course(self):
+        self.__marks.append(-1)
 
     def get_marks(self):
         return self.__marks
